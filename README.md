c829647a20c2a1fedf678e29f60465232f741cee
xiaoguiking <king1129371217@163.com>
2020-04-03 18:50:56 +0800

    context

--- README.md
+++ README.md
@@ -53,3 +53,86 @@ react hooks
 效果：
 - 生成config 具体文件
 - package.json/dependencies 增加很多文件
+  
+
+## 第二章 React 特性
+
+  - Context
+  - ContextType
+  - lazy
+  - Supspense
+  - memo
+
+### 01  Context实现跨层级的组件数据传递(包括多个Context)
+
+**Context定义**
+Context 提供了一种方式，能够让数据在组件树中传递而不必一级一级手动传递
+
+> 生产者爷爷级  动态实现数据
+
+```js
+import React, {Component, createContext} from 'react';
+const BatteryContext = createContext();
+const OnlineContext = createContext();
+
+class App extends Component {
+        state = {
+        battery: 60,
+        online: false
+    };
+    render(){
+        const { battery, online } = this.state;
+    return (  
+      <BatteryContext.Provider value={battery}>
+      <OnlineContext.Provider value={online}>
+      <button onClick={()=> this.setState({battery: battery -1})}>
+      Press
+      </button>
+      <button onClick={()=> this.setState({online: !online})}>
+      Switch
+      </button>
+      <Middle />
+       </OnlineContext.Provider>
+      </BatteryContext.Provider>
+    );
+    }
+}
+```
+> 中间级 父级
+
+```js
+class Middle extends Component {
+    render() {
+        return (
+            <Leaf />
+        )
+    }
+}
+```
+> 消费者 孙子级  消费者包裹一个函数  加入动态效果
+
+```js
+class Leaf extends Component {
+    render () {
+        return (
+            <BatteryContext.Consumer>
+            {
+                battery => (
+                    <OnlineContext.COnsumer>
+                    { online => <h1>Battery: {battery}, Online: {String(online)}</h1>}
+                    </OnlineContext.COnsumer>
+                )
+            }
+            </BatteryContext.Consumer>
+        )
+    }
+}
+```
+
+> 页面效果展示  Battery： 60
+
+### 02  静态属性ContextType访问跨层级组件的数据
+
+### 03  Lazy与Suspense实现延迟加载
+
+### 04  Memo实现指定组件进行渲染
--- train-ticket-hooks/src/App.js
+++ train-ticket-hooks/src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, createContext } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BatteryContext = createContext();
+const OnlineContext = createContext();
+
+// 三级组件 消费者包裹一个函数
+class Leaf extends Component {
+  render() {
+    return (
+      <BatteryContext.Consumer>
+        {
+          battery => (
+            <OnlineContext.Consumer>
+            {
+              online => (<h1>Battery: {battery}, Online: {String(online)}</h1>)
+           }
+            </OnlineContext.Consumer>
+          )
+        }
+      </BatteryContext.Consumer>
+    );
+  }
 }
+
+// 中间组件
+class Middle extends Component {
+
+  render() {
+    return (
+      <Leaf />
+    );
+  }
+}
+
+// 生产者
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      battery: 60,
+      online: false,
+    }
+  };
+  render() {
+    const { battery, online } = this.state;
+    console.log(online, 'on');
+    return (
+      <BatteryContext.Provider value={battery}>
+        <OnlineContext.Provider value={online}>
+          <button onClick={() => this.setState({ battery: battery - 1 })}>
+            Press
+      </button>
+          <button onClick={() => this.setState({ online: !online })}>
+            Switch
+      </button>
+        </OnlineContext.Provider>
+        <Middle />
+      </BatteryContext.Provider>
+    );
+  }
+}
+
+
 
 export default App;
